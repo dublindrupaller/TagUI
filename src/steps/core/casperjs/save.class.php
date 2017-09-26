83b442f5fcc8340a2a3e193bedf01e6236a84c30
gus geraghty <geraghty.fergus@gmail.com>
2017-09-26 18:43:40 +0200

    code tidy up

--- src/steps/core/casperjs/save.class.php
+++ src/steps/core/casperjs/save.class.php
@@ -58,28 +58,3 @@ class save extends step {
     else return "{techo('".$raw_intent."');".beg_tx($params). "save_text('',".$twb.".fetchText(tx('" . $params . "')).trim());".end_tx($params);
   }
 }
-
-// notes from GUS
-// The above depends on the assumption that the checking of $params not being empty happens before the call to the parseIntent() method
-// same goes for the sikuli check and establishing $twb.
-
-
-
-/*  copying in the original php function from tagui_parse.php for john to review oop version.
-
-function save_intent($raw_intent) {
-  $twb = $GLOBALS['tagui_web_browser'];
-  $params = trim(substr($raw_intent." ",1+strpos($raw_intent." "," ")));
-  $param1 = trim(substr($params,0,strpos($params," to "))); $param2 = trim(substr($params,4+strpos($params," to ")));
-  if ((strtolower($params) == "page") or (strtolower($param1) == "page")) {
-    if (strpos($params," to ")!==false) return "{techo('".$raw_intent."');\nsave_text('".abs_file($param2)."',".$twb.".getHTML());}".end_fi()."\n";
-    else return "{techo('".$raw_intent."');\nsave_text('',".$twb.".getHTML());}".end_fi()."\n"; 
-  }
-  if ($params == "") echo "ERROR - " . current_line() . " target missing for " . $raw_intent . "\n";
-  else if (strpos($params," to ")!==false) return "{techo('".$raw_intent."');".beg_tx($param1). "save_text('".abs_file($param2)."',".$twb.".fetchText(tx('".$param1."')).trim());".end_tx($param1); 
-  else return "{techo('".$raw_intent."');".beg_tx($params). "save_text('',".$twb.".fetchText(tx('" . $params . "')).trim());".end_tx($params);
-}
-
-
-*/
-
