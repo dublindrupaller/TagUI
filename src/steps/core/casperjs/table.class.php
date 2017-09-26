a5d008ce121255dad96b834657d8e6c6671250de
gus geraghty <geraghty.fergus@gmail.com>
2017-09-26 18:44:49 +0200

    code tidy up

--- src/steps/core/casperjs/table.class.php
+++ src/steps/core/casperjs/table.class.php
@@ -56,23 +56,3 @@ class table extends step {
   }
 }
 
-// notes from GUS
-// The above depends on the assumption that the checking of $params not being empty happens before the call to the parseIntent() method
-// same goes for the sikuli check and establishing $twb.
-
-
-
-/*  copying in the original php function from tagui_parse.php for john to review oop version.
-
-function table_intent($raw_intent) {
-  $twb = $GLOBALS['tagui_web_browser'];
-  $params = trim(substr($raw_intent." ",1+strpos($raw_intent." "," ")));
-  $param1 = trim(substr($params,0,strpos($params," to "))); $param2 = trim(substr($params,4+strpos($params," to ")));
-  if ($params == "") echo "ERROR - " . current_line() . " target missing for " . $raw_intent . "\n";
-  else if (strpos($params," to ")!==false) return "{techo('".$raw_intent."');".beg_tx($param1). "save_table('".abs_file($param2)."',tx('".$param1."'));".end_tx($param1); 
-  else return "{techo('".$raw_intent."');".beg_tx($params)."save_table('',tx('".$params."'));".end_tx($params);
-}
-
-
-
-*/
