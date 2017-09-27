821de0158eb6e8118de04c2838ad16531eb0b632
gus geraghty <geraghty.fergus@gmail.com>
2017-09-27 16:24:46 +0200

    Update hover.class.php

--- src/steps/core/casperjs/hover.class.php
+++ src/steps/core/casperjs/hover.class.php
@@ -6,9 +6,12 @@
 
 /**
  *  hover class which is a child of step
- *  The class contains two methods:
+ *
+ *  The class contains four methods:
+ *  - __construct
  *  - public getIntent()
  *  - public parseIntent()
+ *  - public get_header_js()
  */
 
 class hover extends step {
