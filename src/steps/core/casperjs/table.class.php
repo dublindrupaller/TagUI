66818938a64b5005fd1ad550179e82928d383dd5
gus geraghty <geraghty.fergus@gmail.com>
2017-09-27 16:30:03 +0200

    Update table.class.php

--- src/steps/core/casperjs/table.class.php
+++ src/steps/core/casperjs/table.class.php
@@ -6,9 +6,12 @@
 
 /**
  *  table class which is a child of step
- *  The class contains two methods:
+ *
+ *  The class contains four methods:
+ *  - __construct
  *  - public getIntent()
  *  - public parseIntent()
+ *  - public get_header_js()
  */
 
 class table extends step {  
