02130410f97c0bc686a2af2240257585015369a6
gus geraghty <geraghty.fergus@gmail.com>
2017-09-27 16:02:13 +0200

    adding in get_header_js() method

--- src/steps/core/casperjs/check.class.php
+++ src/steps/core/casperjs/check.class.php
@@ -48,13 +48,3 @@ class check extends step {
     $params = trim(substr($raw_intent." ",1+strpos($raw_intent." "," ")));
     $params = str_replace("||"," JAVASCRIPT_OR ",$params); // to handle conflict with "|" delimiter
     $param1 = trim(substr($params,0,strpos($params,"|"))); 
-    $param2 = trim(substr($params,1+strpos($params,"|")));
-    $param3 = trim(substr($param2,1+strpos($param2,"|"))); 
-    $param2 = trim(substr($param2,0,strpos($param2,"|")));
-    $param1 = str_replace(" JAVASCRIPT_OR ","||",$param1); // to restore back "||" that were replaced
-    $param2 = str_replace(" JAVASCRIPT_OR ","||",$param2); 
-    $param3 = str_replace(" JAVASCRIPT_OR ","||",$param3);
-    if (substr_count($params,"|")!=2) echo "ERROR - " . current_line() . " if/true/false missing for " . $raw_intent . "\n"; 
-    else return "{".parse_condition("if ".$param1)."\nthis.echo(".$param2.");\nelse this.echo(".$param3.");}".end_fi()."\n";
-  }
-}
