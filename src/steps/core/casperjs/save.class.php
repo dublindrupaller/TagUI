1256b9b043a70bad95609e17f32f0075b6b4a598
gus geraghty <geraghty.fergus@gmail.com>
2017-09-27 16:13:30 +0200

    adding in get_header_js() method

--- src/steps/core/casperjs/save.class.php
+++ src/steps/core/casperjs/save.class.php
@@ -5,13 +5,13 @@
  */
 
 /**
- *  save class which is a child of step
+ *  select class which is a child of step
  *  The class contains two methods:
  *  - public getIntent()
  *  - public parseIntent()
  */
 
-class save extends step {  
+class select extends step {
     
   /**
    * Construct wrapper object   
@@ -24,7 +24,7 @@ class save extends step {
    */
   public function getIntent($intent) {    
 
-    if (substr($intent,0,5)=="save ") {
+    if ((substr($intent,0,7)=="select ") or (substr($intent,0,7)=="choose ")){
       return $this->intent;
     }    
     return FALSE;
@@ -48,33 +48,40 @@ class save extends step {
   public function parseIntent($intent, $raw_intent, $twb, $sikuli=FALSE) {     
     // TODO: $params is passed as an array but sent to casperjs code and sikuli output as a string   
     $params = trim(substr($raw_intent." ",1+strpos($raw_intent." "," ")));
-    $param1 = trim(substr($params,0,strpos($params," to "))); $param2 = trim(substr($params,4+strpos($params," to ")));
-    if ((strtolower($params) == "page") or (strtolower($param1) == "page")) {
-      if (strpos($params," to ")!==false) return "{techo('".$raw_intent."');\nsave_text('".abs_file($param2)."',".$twb.".getHTML());}".end_fi()."\n";
-      else return "{techo('".$raw_intent."');\nsave_text('',".$twb.".getHTML());}".end_fi()."\n"; 
-    }
-    if ($params == "") echo "ERROR - " . current_line() . " target missing for " . $raw_intent . "\n";
-    else if (strpos($params," to ")!==false) return "{techo('".$raw_intent."');".beg_tx($param1). "save_text('".abs_file($param2)."',".$twb.".fetchText(tx('".$param1."')).trim());".end_tx($param1); 
-    else return "{techo('".$raw_intent."');".beg_tx($params). "save_text('',".$twb.".fetchText(tx('" . $params . "')).trim());".end_tx($params);
+    $param1 = trim(substr($params,0,strpos($params," as "))); 
+    $param2 = trim(substr($params,4+strpos($params," as ")));
+
+    if (is_sikuli($param1) and is_sikuli($param2)) {
+      $abs_param1 = abs_file($param1); $abs_intent = str_replace($param1,$abs_param1,$raw_intent);
+      $abs_param2 = abs_file($param2); $abs_intent = str_replace($param2,$abs_param2,$abs_intent);
+      return call_sikuli($abs_intent,$abs_param1);
+    } // use sikuli visual automation as needed
+
+    if (($param1 == "") or ($param2 == "")) echo "ERROR - " . current_line() . " target/option missing for " . $raw_intent . "\n";
+
+    else return "{techo('".$raw_intent."');".beg_tx($param1)."var select_locator = tx('".$param1."');\n"."if (is_xpath_selector(select_locator.toString().replace('xpath selector: ','')))\n"."select_locator = select_locator.toString().substring(16);\n".$twb.".selectOptionByValue(select_locator,'".$param2."');".end_tx($param1);
   }
+
 
   
   public function get_header_js() {
     $js = <<<TAGUI
-function save_intent(raw_intent) {
+function select_intent(raw_intent) {
 var params = ((raw_intent + ' ').substr(1+(raw_intent + ' ').indexOf(' '))).trim();
-var param1 = (params.substr(0,params.indexOf(' to '))).trim();
-var param2 = (params.substr(4+params.indexOf(' to '))).trim();
-if ((params.toLowerCase() == 'page') || (param1.toLowerCase() == 'page')) {
-if (params.indexOf(' to ') > -1) return "save_text('" + abs_file(param2) + "',this.getHTML())";
-else return "save_text('',this.getHTML())";}
-if (params == '') return "this.echo('ERROR - target missing for " + raw_intent + "')";
-else if (params.indexOf(' to ') > -1)
-{if (check_tx(param1)) return "save_text('" + abs_file(param2) + "',this.fetchText(tx('" + param1 + "')).trim())";
+var param1 = (params.substr(0,params.indexOf(' as '))).trim();
+var param2 = (params.substr(4+params.indexOf(' as '))).trim();
+if (is_sikuli(param1) && is_sikuli(param2)) {
+var abs_param1 = abs_file(param1); var abs_intent = raw_intent.replace(param1,abs_param1);
+var abs_param2 = abs_file(param2); abs_intent = abs_intent.replace(param2,abs_param2);
+return call_sikuli(abs_intent,abs_param1);} // use sikuli visual automation as needed
+if ((param1 == '') || (param2 == '')) return "this.echo('ERROR - target/option missing for " + raw_intent + "')";
+else if (check_tx(param1)) return "var select_locator = tx('" + param1 + "'); if (is_xpath_selector(select_locator.toString().replace('xpath selector: ',''))) select_locator = select_locator.toString().substring(16); this.selectOptionByValue(select_locator,'" + param2 + "');";
 else return "this.echo('ERROR - cannot find " + param1 + "')";}
-else {if (check_tx(params)) return "save_text('',this.fetchText(tx('" + params + "')).trim())";
-else return "this.echo('ERROR - cannot find " + params + "')";}}
 TAGUI;
     return $js;
   }    
+
+
 }
+
+class_alias('select', 'choose');
