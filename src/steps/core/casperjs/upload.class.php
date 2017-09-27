f27d80cc72b17b53f2d5a43a8662ed3885f6a70a
gus geraghty <geraghty.fergus@gmail.com>
2017-09-27 16:18:26 +0200

    adding in get_header_js() method

--- src/steps/core/casperjs/upload.class.php
+++ src/steps/core/casperjs/upload.class.php
@@ -51,4 +51,18 @@ class upload extends step {
     if (($param1 == "") or ($param2 == "")) echo "ERROR - " . current_line() . " filename missing for " . $raw_intent . "\n"; 
     else return "{techo('".$raw_intent."');".beg_tx($param1).$twb.".page.uploadFile(tx('".$param1."'),'".abs_file($param2)."');".end_tx($param1);
   }
+
+
+  public function get_header_js() {
+    $js = <<<TAGUI
+function upload_intent(raw_intent) {
+var params = ((raw_intent + ' ').substr(1+(raw_intent + ' ').indexOf(' '))).trim();
+var param1 = (params.substr(0,params.indexOf(' as '))).trim();
+var param2 = (params.substr(4+params.indexOf(' as '))).trim();
+if ((param1 == '') || (param2 == '')) return "this.echo('ERROR - filename missing for " + raw_intent + "')";
+else if (check_tx(param1)) return "this.page.uploadFile(tx('" + param1 + "'),'" + abs_file(param2) + "')";
+else return "this.echo('ERROR - cannot find " + param1 + "')";}
+TAGUI;
+    return $js;
+  }           
 }
