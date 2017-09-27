0d9bb8582243814f6b16d638e7291035f6cb4c1b
gus geraghty <geraghty.fergus@gmail.com>
2017-09-27 16:31:50 +0200

    Update test.class.php

--- src/steps/core/casperjs/test.class.php
+++ src/steps/core/casperjs/test.class.php
@@ -6,9 +6,12 @@
 
 /**
  *  test class which is a child of step
- *  The class contains two methods:
+ *
+ *  The class contains four methods:
+ *  - __construct
  *  - public getIntent()
  *  - public parseIntent()
+ *  - public get_header_js()
  */
 
 class test extends step {  
