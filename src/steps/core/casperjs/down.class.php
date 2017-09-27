58004ebb41a882db28dabea386c61a577f4b229b
gus geraghty <geraghty.fergus@gmail.com>
2017-09-27 16:05:38 +0200

    adding in get_header_js() method

--- src/steps/core/casperjs/down.class.php
+++ src/steps/core/casperjs/down.class.php
@@ -52,6 +52,18 @@ class down extends step {
 
     if (($param1 == "") or ($param2 == "")) echo "ERROR - " . current_line() . " url/filename missing for " . $raw_intent . "\n"; 
     else return "{techo('".$raw_intent."');\n".$twb.".download('".$param1."','".abs_file($param2)."');}".end_fi()."\n";
-  }    
-  
+  } 
+
+
+  public function get_header_js() {
+    $js = <<<TAGUI
+function down_intent(raw_intent) {
+var params = ((raw_intent + ' ').substr(1+(raw_intent + ' ').indexOf(' '))).trim();
+var param1 = (params.substr(0,params.indexOf(' to '))).trim();
+var param2 = (params.substr(4+params.indexOf(' to '))).trim();
+if ((param1 == '') || (param2 == '')) return "this.echo('ERROR - url/filename missing for " + raw_intent + "')";
+else return "this.download('" + param1 + "','" + abs_file(param2) + "')";}
+TAGUI;
+   return $js;
+  }
 }
