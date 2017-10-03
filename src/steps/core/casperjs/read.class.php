4243e96e84e821a1255c7e7fbd325ff3608e878b
gus geraghty <geraghty.fergus@gmail.com>
2017-10-03 09:46:36 +0200

    added headerjs method to read step class

--- src/steps/core/casperjs/read.class.php
+++ src/steps/core/casperjs/read.class.php
@@ -6,9 +6,10 @@
 
 /**
  *  read class which is a child of step
- *  The class contains two methods:
+ *  The class contains three methods:
  *  - public getIntent()
  *  - public parseIntent()
+ *  - public get_header_js() 
  */
 
 class read extends step {
@@ -53,7 +54,20 @@ class read extends step {
     if (($param1 == "") or ($param2 == "")) echo "ERROR - " . current_line() . " target/variable missing for " . $raw_intent . "\n"; 
   }
 
+  public function getHeaderJs() {
+    $js = <<<TAGUI
+function read_intent(raw_intent) {
+var params = ((raw_intent + ' ').substr(1+(raw_intent + ' ').indexOf(' '))).trim();
+var param1 = (params.substr(0,params.indexOf(' to '))).trim();
+var param2 = (params.substr(4+params.indexOf(' to '))).trim();
+if ((param1.toLowerCase() == 'page') && (param2 !== '')) return param2 + " = this.getHTML()";
+if ((param1 == '') || (param2 == '')) return "this.echo('ERROR - target/variable missing for " + raw_intent + "')";
+else if (check_tx(param1)) return param2 + " =  this.fetchText(tx('" + param1 + "')).trim()";
+else return "this.echo('ERROR - cannot find " + param1 + "')";}
+TAGUI;
+    return $js;
+  }
+  
 }
 
 class_alias('read', 'fetch');
-
