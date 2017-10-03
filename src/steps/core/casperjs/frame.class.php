1f1d593097819db6a426890f1b3e2b2baa0327a5
gus geraghty <geraghty.fergus@gmail.com>
2017-10-03 10:24:25 +0200

    Update frame.class.php

--- src/steps/core/casperjs/frame.class.php
+++ src/steps/core/casperjs/frame.class.php
@@ -6,12 +6,10 @@
 
 /**
  *  frame class which is a child of step
- *
- *  The class contains four methods:
- *  - __construct
+ *  The class contains three methods:
  *  - public getIntent()
  *  - public parseIntent()
- *  - public get_header_js()
+ *  - public getHeaderJs()
  */
 
 class frame extends step {  
@@ -63,8 +61,8 @@ class frame extends step {
       $GLOBALS['inside_frame']=1; return "{techo('".$raw_intent."');\ncasper.withFrame('".$params."', function() {\n";
     }
   }
-
-  public function get_header_js() {
+  
+  public function getHeaderJs() {
     $js = <<<TAGUI
 function frame_intent(raw_intent) {
 return "this.echo('ERROR - step not supported in live mode, it is meant for trying single steps')";}
