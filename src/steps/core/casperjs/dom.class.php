4af4d55724da4806108e0039e172db5cf61b3aeb
gus geraghty <geraghty.fergus@gmail.com>
2017-10-03 09:37:51 +0200

    adding headerjs method to dom step

--- src/steps/core/casperjs/dom.class.php
+++ src/steps/core/casperjs/dom.class.php
@@ -6,9 +6,10 @@
 
 /**
  *  dom class which is a child of step
- *  The class contains two methods:
+ *  The class contains three methods:
  *  - public getIntent()
  *  - public parseIntent()
+ *  - public getHeaderJs()
  */
 
 class dom extends step {  
@@ -50,4 +51,14 @@ class dom extends step {
     else return "dom_result = ".$twb.".evaluate(function() {".$params."});".end_fi()."\n";
   }
 
+
+  public function getHeaderJs() {
+    $js = <<<TAGUI
+function dom_intent(raw_intent) {
+var params = ((raw_intent + ' ').substr(1+(raw_intent + ' ').indexOf(' '))).trim();
+if (params == '') return "this.echo('ERROR - statement missing for " + raw_intent + "')";
+else return "dom_result = this.evaluate(function(dom_json) {" + params + "}, dom_json)";}
+TAGUI;
+    return $js;
+  }
 }
